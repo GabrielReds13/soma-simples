281ff6e3b0c865044fee432ba98df1282794e949
GabrielReds13 <speedckprod4007@gmail.com>
2024-10-14 21:58:13 -0300

    Fix: main.js error

--- main.js
+++ main.js
@@ -1,6 +1,3 @@
-// import
-import { sum } from "./script/sum";
-
 // elements
 const resultSum = document.getElementById("result-sum");
 
@@ -10,11 +7,36 @@ const inputNum2 = document.getElementById("input-num2");
 const btnSubmit1 = document.getElementById("submit-1");
 const btnSubmit2 = document.getElementById("submit-2");
 
-async function sumNums() {
-    alert("Oi")
-    // resultSum.innerText = await sum(inputNum1.value, inputNum2.value);
+// code
+async function sum(num1, num2) {
+    try {
+        // verify value
+        if(num1 && num2) {
+            // convert to num
+            num1 = Number(num1);
+            num2 = Number(num2);
+
+            // verify if num is not a number
+            if (isNaN(num1) === false && isNaN(num2) === false) {
+                // result
+                return `Resultado: ${num1 + num2}`;
+            } return "Só é possível somar números."
+        } else return "Verifique se os campos estão preenchidos."
+    } catch (error) {
+        // error message
+        return "Erro ao somar os números.";
+    }
 }
 
-btnSubmit1.addEventListener("click", () => {
-    sumNums()
-})
+// set result
+async function sumNums() {
+    resultSum.innerText = await sum(inputNum1.value, inputNum2.value);
+}
+
+// clear all fields
+async function clearFields() {
+    resultSum.innerText = "Escolha e some números.";
+    inputNum1.value = ""; 
+    inputNum2.value = "";
+}
+
